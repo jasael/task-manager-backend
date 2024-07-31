1f12f94a7e1ae135e64ba50e5b183d4919853e9f
Jasael Palma <programadorjp2001@gmail.com>
2024-07-30 21:52:07 -0600

    configuring states crud

--- src/states/states.service.ts
+++ src/states/states.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateStateDto } from './dto/create-state.dto';
 import { UpdateStateDto } from './dto/update-state.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { State } from './entities/state.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StatesService {
-  create(createStateDto: CreateStateDto) {
-    return 'This action adds a new state';
+  constructor(
+    @InjectRepository(State)
+    private stateRepository: Repository<State>,
+  ) {}
+  async create(createStateDto: CreateStateDto) {
+    const state = this.stateRepository.create(createStateDto);
+
+    await this.stateRepository.save(state);
   }
 
   findAll() {
-    return `This action returns all states`;
+    return this.stateRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} state`;
+    return this.stateRepository.findOneBy({ id });
   }
 
-  update(id: number, updateStateDto: UpdateStateDto) {
-    return `This action updates a #${id} state`;
+  async update(id: number, updateStateDto: UpdateStateDto) {
+    const response = await this.stateRepository.update(
+      {
+        id,
+      },
+      updateStateDto,
+    );
+
+    if (!response.affected) {
+      throw new NotFoundException(
+        `No se ha encontrado el estado con id: ${id}`,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} state`;
+  async remove(id: number) {
+    const response = await this.stateRepository.softDelete(id);
+
+    if (!response.affected) {
+      throw new NotFoundException(
+        `No se ha encontrado el estado con id: ${id}`,
+      );
+    }
   }
 }
